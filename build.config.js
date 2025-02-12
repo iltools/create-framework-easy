57ddb79ef259a483ce1331ba5e38ff66a760c70f
iltools <398778994@qq.com>
2025-02-12 14:37:50 +0800

    teat: action

--- build.config.js
+++ build.config.js
@@ -1,11 +1,3 @@
-/*
- * @Author: iltools 398778994@qq.com
- * @Date: 2025-02-12 14:36:36
- * @LastEditors: iltools 398778994@qq.com
- * @LastEditTime: 2025-02-12 14:37:34
- * @FilePath: \create-framework-easy\build.config.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 export default {
     failOnWarn: false
 };
